d94972154fb4535ba62edac3602957a2ff21eb18
Jin Liu <iamjincn@outlook.com>
2025-07-31 16:13:07 +1000

    fix: reorder import statement for consistency

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This module provides a function to find the floor of a number."""
-
-
 import math
+
+
 def floor(n: float) -> int:
     """Find the floor of a number."""
     return math.floor(n)
