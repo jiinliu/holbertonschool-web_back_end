9703acc9a472fa556b5d845173f1efe1577ad961
Jin Liu <iamjincn@outlook.com>
2025-07-31 16:31:02 +1000

    fix: remove redundant blank lines in the to_str module

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-
+"""Module for converting a float to its string representation."""
 def to_str(n: float) -> str:
     """Returns the string representation of a float n."""
     return str(n)
