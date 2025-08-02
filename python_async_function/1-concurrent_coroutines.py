59f0fd4fe3974686590638f725091e62fe060cdd
Jin Liu <iamjincn@outlook.com>
2025-08-02 22:55:12 +1000

    feat: implement wait_n function for asynchronous delay collection

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module for running asynchronous random delays and collecting results.
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Run wait_random n times and return list of delays in order of completion..
+    """
+    tasks = []
+    for _ in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
