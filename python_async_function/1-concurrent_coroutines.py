3ccd5dd84c38e36daef857ad07c95c116a983800
Jin Liu <iamjincn@outlook.com>
2025-08-02 22:56:43 +1000

    fix: remove extra newline in import section for consistency

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Run wait_random n times and return list of delays in order of completion..
