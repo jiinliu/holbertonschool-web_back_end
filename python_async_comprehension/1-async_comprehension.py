f857d87527bdfc1d8aa30a343b21c531013f98aa
Jin Liu <iamjincn@outlook.com>
2025-08-04 18:02:00 +1000

    fix: improve formatting and import statement in async_comprehension.py for clarity

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,12 @@
 """This module provides a coroutine to asynchronously collect 10 random numbers into a list."""
 
 from typing import List
-import async_generator
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collect 10 random floats from async_generator and return them as a list."""
+    """
+    Collect 10 random floats from async_generator and return them as a list.
+    """
     return [num async for num in async_generator()]
