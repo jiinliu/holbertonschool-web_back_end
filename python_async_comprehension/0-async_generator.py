1add1927f1b5be6c1d1ca4014c8cab55ae0f438d
Jin Liu <iamjincn@outlook.com>
2025-08-04 17:57:49 +1000

    fix: remove extra blank line for consistency in async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module provides an async generator that yields random numbers."""
-
 import asyncio
 import random
 from typing import Generator
+
 
 async def async_generator() -> Generator[float, None, None]:
     """
