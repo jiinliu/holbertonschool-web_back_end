ea0724f69d78f9cb2228546636691de476620b31
Jin Liu <iamjincn@outlook.com>
2025-08-10 23:18:04 +1000

    fix: simplify async_comprehension calls in measure_runtime for clarity

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -14,11 +14,6 @@ async def measure_runtime() -> float:
     and return it.
     """
     start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end = time.perf_counter()
     return end - start
