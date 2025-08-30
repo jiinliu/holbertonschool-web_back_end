f94f4c23d74bc57f54f5ac0fdb26776798ef897b
Jin Liu <iamjincn@outlook.com>
2025-08-30 21:50:18 +1000

    feat: add HolbertonClass and StudentHolberton classes with example usage

---
+++ ES6_classes/9-hoisting.js
@@ -0,0 +1,34 @@
+// 9-hoisting.js
+
+class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+  get year() { return this._year; }
+  get location() { return this._location; }
+}
+
+class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+  get fullName() { return `${this._firstName} ${this._lastName}`; }
+  get holbertonClass() { return this._holbertonClass; }
+  get fullStudentDescription() {
+    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+export default [
+  new StudentHolberton('Guillaume', 'Salva', class2020),
+  new StudentHolberton('John', 'Doe', class2020),
+  new StudentHolberton('Albert', 'Clinton', class2019),
+  new StudentHolberton('Donald', 'Bush', class2019),
+  new StudentHolberton('Jason', 'Sandler', class2019),
+];
