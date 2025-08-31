203e0ac75d83273c84c58aec92d70f0d7427b299
Jin Liu <iamjincn@outlook.com>
2025-08-31 17:39:49 +1000

    fix: correct spelling of 'Serena' in getListStudents return data

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudentsreturn() {
     return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serana', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
 }
