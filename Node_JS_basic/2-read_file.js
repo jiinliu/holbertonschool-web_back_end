0df7a9526874c557b63f29e2da7c0abeb7e57771
Jin Liu <iamjincn@outlook.com>
2025-09-05 18:33:50 +1000

    refactor: simplify code by using arrow functions and template literals in countStudents function

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,35 +8,32 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 
-  const lines = data.split(/\r?\n/).filter(function (line) {
-    return line.trim() !== '';
-  });
+  const lines = data.split(/\r?\n/).filter((line) => line.trim() !== '');
 
   if (lines.length <= 1) {
     console.log('Number of students: 0');
     return;
   }
 
-  const rows = lines.slice(1).map(function (line) {
-    return line.split(',').map(function (s) { return s.trim(); });
-  });
+  const rows = lines.slice(1).map((line) =>
+    line.split(',').map((s) => s.trim())
+  );
 
-  console.log('Number of students: ' + rows.length);
+  console.log(`Number of students: ${rows.length}`);
 
   const groups = {};
-  rows.forEach(function (cols) {
-    const firstname = cols[0];
-    const field = cols[3];
+  rows.forEach(([firstname, , , field]) => {
     if (!firstname || !field) return;
-
     if (!groups[field]) groups[field] = [];
     groups[field].push(firstname);
   });
 
-  Object.keys(groups).sort().forEach(function (field) {
-    const list = groups[field].join(', ');
-    console.log('Number of students in ' + field + ': ' + groups[field].length + '. List: ' + list);
-  });
+  Object.keys(groups)
+    .sort()
+    .forEach((field) => {
+      const list = groups[field].join(', ');
+      console.log(`Number of students in ${field}: ${groups[field].length}. List: ${list}`);
+    });
 }
 
 module.exports = countStudents;
