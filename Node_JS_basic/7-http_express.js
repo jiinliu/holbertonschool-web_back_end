dff8e46bd4c5b625d40a5f7d8252a7490d313363
Jin Liu <iamjincn@outlook.com>
2025-09-06 19:18:55 +1000

    fix: refactor response handling in routes for clarity and consistency

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,20 +1,25 @@
+// 7-http_express.js
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
 
-app.get('/',(req, res) => res.type('text').send('Hello Holberton School!'))
-
-app.get('/students', async (req, res ) => {
-    res.write('This is the list of our students\n');
-    try {
-        const out = await countStudents(process.argv[2]);
-        res.send(out);
-    } catch (error) {
-        res.send('Cannot load the database');
-    }
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(12345);
-module.exports = app;
+app.get('/students', async (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  const prefix = 'This is the list of our students\n';
+  try {
 
+    const out = await countStudents(process.argv[2]);
+    res.end(`${prefix}${out}`);
+  } catch {
+    res.end(`${prefix}Cannot load the database`);
+  }
+});
+
+app.listen(1245);
+module.exports = app;
