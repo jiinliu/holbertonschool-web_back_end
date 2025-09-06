102812ff186277b29c9c55959325eab6288fbaca
Jin Liu <iamjincn@outlook.com>
2025-09-06 19:25:36 +1000

    fix: streamline response handling in root and students routes

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,23 +1,17 @@
-// 7-http_express.js
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
-});
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 
 app.get('/students', async (req, res) => {
-  res.set('Content-Type', 'text/plain');
   const prefix = 'This is the list of our students\n';
   try {
-
     const out = await countStudents(process.argv[2]);
-    res.end(`${prefix}${out}`);
+    return res.send(`${prefix}${out}`);
   } catch {
-    res.end(`${prefix}Cannot load the database`);
+    return res.send(`${prefix}Cannot load the database`);
   }
 });
 
