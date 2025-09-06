747e8eaf10fd98b9c307a1251fd5b87066afee2a
Jin Liu <iamjincn@outlook.com>
2025-09-06 19:29:46 +1000

    fix: improve response handling in root route and add error parameter in students route

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,14 +3,18 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
 
 app.get('/students', async (req, res) => {
+  res.set('Content-Type', 'text/plain');
   const prefix = 'This is the list of our students\n';
   try {
     const out = await countStudents(process.argv[2]);
     return res.send(`${prefix}${out}`);
-  } catch {
+  } catch (error) {
     return res.send(`${prefix}Cannot load the database`);
   }
 });
